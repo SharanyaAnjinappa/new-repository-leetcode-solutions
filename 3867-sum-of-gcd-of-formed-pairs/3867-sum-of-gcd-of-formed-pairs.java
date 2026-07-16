cb58ebd62b62812c6b3068b982aea6eb4ae9a855
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-07-16 06:08:57 +0530

    Time: 55 ms (78.5%), Space: 108.2 MB (47%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -18,7 +18,10 @@ class Solution {
         return ans;
     }
     public int gcd(int i,int j){
-        if(j==0){return i;}
-        return gcd(j,i%j);
+        while(j!=0){
+            int temp=i%j;
+            i=j;
+            j=temp;
+        }return i;
     }
 }
