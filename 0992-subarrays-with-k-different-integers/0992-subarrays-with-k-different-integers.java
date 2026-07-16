f3d8631d91c388539fdf3282d1da9a976f37a7b3
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-07-16 22:55:54 +0530

    Time: 4 ms (99.97%), Space: 49.2 MB (86.96%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -5,14 +5,39 @@ class Solution {
     public int atmost(int[] nums,int k){
         int l=0;
         int ans=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
+        int distint=0;
+        int[] arr=new int[nums.length+1];
         for(int r=0;r<nums.length;r++){
-            map.put(nums[r],map.getOrDefault(nums[r],0)+1);
-            while(map.size()>k){
-                map.put(nums[l],map.get(nums[l])-1);
-                if(map.get(nums[l])==0){map.remove(nums[l]);}
+            if(arr[nums[r]]++==0){distint++;}
+            while(distint>k){
+                if(--arr[nums[l]]==0){distint--;}
                 l++;
-            }ans+=r-l+1;
+            }
+            ans+=r-l+1;
         }return ans;
     }
 }
+
+
+ /*   private int atMost(int[] nums, int k) {
+        int[] freq = new int[nums.length + 1];
+        int left = 0, distinct = 0, count = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            if (freq[nums[right]]++ == 0) {
+                distinct++;
+            }
+            
+            while (distinct > k) {
+                if (--freq[nums[left]] == 0) {
+                    distinct--;
+                }
+                left++;
+            }
+            
+            count += right - left + 1;
+        }
+        
+        return count;
+    }
+}*/
