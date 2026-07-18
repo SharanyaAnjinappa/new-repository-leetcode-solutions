b630d25e64803d79329caaf6d834eaec242e108f
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-07-18 06:26:30 +0530

    Time: 2 ms (92.15%), Space: 47.7 MB (57.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=0;
+       /* int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 int temp=nums[i];
@@ -8,6 +8,15 @@ class Solution {
                 nums[j]=temp;
                 j++;
             }
+        }*/
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]!=0){
+                nums[l++]=nums[r];
+            }
         }
+         while(l<nums.length){
+                nums[l++]=0;
+            }
     }
 }
