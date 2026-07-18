4fe5577f6be5c081037037a9a746d251ca8a0662
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-07-18 21:54:23 +0530

    Time: 4 ms (82.67%), Space: 42.4 MB (5.1%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -5,7 +5,7 @@ class Solution {
         while(l<=r){
             long s=l*l+r*r;
             if(s==c){return true;}
-            if(s<c){l++;}
+            else if(s<c){l++;}
             else{r--;}
         }return false;
     }
