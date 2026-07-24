e520c41fe04be8bb4971b676810d9211529481b4
Sharanya.A <eng23cs0183@dsu.edu.in>
2026-07-24 14:00:31 +0530

    Time: 0 ms (100%), Space: 43.3 MB (16.43%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,23 @@
+class Solution {
+
+   /* static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }*/
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
